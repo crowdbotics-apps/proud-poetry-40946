831a97802204b906339928d71a8f8b90742a85a1
Crowdbotics <team@crowdbotics.com>
2023-05-24 02:25:17 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "proud-poetry-40946",
+  url: "https://proud-poetry-40946.botics.co",
+  api: "https://proud-poetry-40946.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
